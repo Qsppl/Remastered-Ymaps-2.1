b8188027d4d4c31c9135cd8b330b81a7d9f2b51b
Tomoe <4.artem.0@gmail.com>
2025-02-25 13:43:46 +0300

    add command-line interface (#1)

--- application/cli/cli.mjs
+++ application/cli/cli.mjs
@@ -1,3 +1,13 @@
-"use strict";
-export default null;
-throw new Error("CLI not implemented!");
+#!/usr/bin/env node
+import { program } from 'commander';
+import data from "../../package.json" with { type: "json" };
+export async function main() {
+    program
+        .version(data.version)
+        .description(data.description);
+}
+// Execute the main function to start the CLI
+main().catch(error => {
+    console.error('Fatal error:', error);
+    process.exit(1);
+});
--- application/cli/cli.mts
+++ application/cli/cli.mts
@@ -1,3 +1,16 @@
-export default null
+#!/usr/bin/env node
 
-throw new Error("CLI not implemented!")
+import { program } from 'commander'
+import data from "../../package.json" with { type: "json" }
+
+export async function main() {
+    program
+        .version(data.version)
+        .description(data.description)
+}
+
+// Execute the main function to start the CLI
+main().catch(error => {
+    console.error('Fatal error:', error)
+    process.exit(1)
+})
